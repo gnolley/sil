d0c0a569fe09ce8c8c862785f552bd033c5fd324
Tom Knowles <thomasa.knowles@gmail.com>
2024-10-24 21:44:54 +1000

    chore: basic hard-coded spir-v compilation

--- tools/stagehand/main.cpp
+++ tools/stagehand/main.cpp
@@ -18,6 +18,28 @@ void GetShader(const std::string& filePath, std::string& shader)
 	shader = stringStream.str();
 }
 
+void CompileShader(std::string& output, const shaderc::Compiler& compiler, const shaderc_shader_kind kind, const std::string& shader, const std::string& name)
+{
+	const shaderc::CompileOptions options{};
+
+	auto preprocessResult = compiler.PreprocessGlsl(shader, kind, name.c_str(), options);
+	std::string preprocessed(preprocessResult.begin(), preprocessResult.end());
+
+	auto compileResult = compiler.CompileGlslToSpv(preprocessed, shaderc_shader_kind::shaderc_glsl_default_vertex_shader, name.c_str(), options);
+	if (compileResult.GetCompilationStatus() != shaderc_compilation_status::shaderc_compilation_status_success)
+	{
+		std::cout << "Error compiling shader " << name << ": " << compileResult.GetErrorMessage() << "\n";
+	}
+	output = { compileResult.begin(), compileResult.end() };
+}
+
+void WriteShader(std::string& compiledShader, std::string outputPath)
+{
+	std::ofstream output(outputPath, std::ios::binary);
+	output << compiledShader;
+	output.close();
+}
+
 int main()
 {
 	std::string vert, frag;
@@ -25,16 +47,12 @@ int main()
 	GetShader("test.frag", frag);
 
 	shaderc::Compiler compiler{};
-	shaderc::CompileOptions options{};
+	std::string vertCompiled, fragCompiled;
+	CompileShader(vertCompiled, compiler, shaderc_shader_kind::shaderc_glsl_vertex_shader, vert, "test");
+	CompileShader(fragCompiled, compiler, shaderc_shader_kind::shaderc_fragment_shader, frag, "test");
 
-	auto preprocessResult = compiler.PreprocessGlsl(vert, shaderc_shader_kind::shaderc_glsl_vertex_shader, "vert", options);
-	std::string preprocessed(preprocessResult.begin(), preprocessResult.end());
-
-	auto compileResult = compiler.CompileGlslToSpv(preprocessed, shaderc_shader_kind::shaderc_glsl_default_vertex_shader, "vert", options);
-	std::string shader (compileResult.begin(), compileResult.end());
-
-	std::cout << "Compiled vert: \n" << shader;
-
+	WriteShader(vertCompiled, "vert.spv");
+	WriteShader(fragCompiled, "frag.spv");
 	return EXIT_SUCCESS;
 
 }
