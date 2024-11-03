1488e332d55b3a87797e889f59b3227b31d8c511
Tom Knowles <thomasa.knowles@gmail.com>
2024-11-03 21:41:24 +1000

    fix: cmake errors

--- cmake/Targets.cmake
+++ cmake/Targets.cmake
@@ -6,7 +6,7 @@ set(BOOTSTRAP_TARGET Sil-Bootstrapper)
 set(COOK_TOOL_TARGET Stagehand)
 set(COOK_TOOL_DIR ${CMAKE_SOURCE_DIR}/tools/stagehand)
 
-set(PLAYBOOK_LIB_TARGET Playbook);
+set(PLAYBOOK_LIB_TARGET Playbook)
 set(PLAYBOOK_LIB_DIR ${CMAKE_SOURCE_DIR}/engine/libs/playbook)
 
 set(BUILD_BUILTIN_ASSETS_TARGET StagehandBuildBuiltin)
--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -10,17 +10,17 @@ endif()
 function(setup_target TARGET_NAME)
 
 	if(${ENABLE_WARNINGS})
-	    target_set_warnings(${ENGINE_SRC_TARGET} ${ENABLE_WARNINGS_AS_ERRORS})
+	    target_set_warnings(${TARGET_NAME} ${TARGET_NAME})
     endif()
 
     if(${ENABLE_LTO})
-        target_enable_lto(${ENGINE_SRC_TARGET})
+        target_enable_lto(${TARGET_NAME})
     endif()
 
     if(${CMAKE_BUILD_TYPE} STREQUAL "DEBUG")
-        target_compile_definitions(${ENGINE_SRC_TARGET} PRIVATE ${SIL_DEBUG_FLAG})
+        target_compile_definitions(${TARGET_NAME} PRIVATE ${SIL_DEBUG_FLAG})
     elseif(${CMAKE_BUILD_TYPE} STREQUAL "RELEASE")
-        target_compile_definitions(${ENGINE_SRC_TARGET} PRIVATE ${SIL_RELEASE_FLAG})
+        target_compile_definitions(${TARGET_NAME} PRIVATE ${SIL_RELEASE_FLAG})
     endif()
 
 endfunction()
